c9486da8d4a406dc8f1319db5c75b7143ce5dbf0
MarioArmas <mario.r.armas@gmail.com>
2022-01-09 14:56:15 -0600

    add local storage

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,28 @@
-import React, { useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Todo from './Todo'
 
+const LOCAL_STORAGE_KEY = "todoApp.todos"
+const TODOS = [{ name: 'ohoie', done: true }, { name: "oaefhoe", done: false }]
+
 export default function TodoList() {
-  const [todos, setTodos] = useState([{ name: 'ohoie', done: true }, { name: "oaefhoe", done: false }])
+  const [todos, setTodos] = useState([])
   const inputName = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
   const addTodo = () => {
     const name = inputName.current.value
     setTodos((prevTodos) => {
       return [...prevTodos, { name, done: false }]
     })
+    inputName.current.value = null
   }
 
   return (
